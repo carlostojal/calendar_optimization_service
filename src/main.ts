b57cdbaf86adf2692492ab8bdc2ba92d14759955
carlostojal <carlos.tojal@hotmail.com>
2023-07-05 16:59:13 +0100

    properly call the optimizer

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,37 @@
 import express, { Express, Request, Response } from 'express';
-import 'dotenv';
+import dotenv from 'dotenv';
 import { CalendarEvent } from './models/CalendarEvent';
 import { GeneticOptimizer } from './controllers/GeneticOptimizer';
+
+dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.post('/optimize', (req: Request, res: Response) => {
-    const events: CalendarEvent[] = req.body.events;
+    const events = req.body;
+
+    let calendarEvents: CalendarEvent[] = [];
+
+    events.forEach((event: any) => {
+        let newEvent: CalendarEvent = new CalendarEvent();
+        newEvent.id = event.id || 0;
+        newEvent.eventName = event.name || "New Event";
+        newEvent.eventDescription = event.description || "";
+        newEvent.eventDate = new Date(event.date);
+        newEvent.isFlexible = event.flexible || true;
+        newEvent.eventDurationMinutes = event.durationMinutes || 30;
+        newEvent.eventDayPeriod = event.preferredDayPeriod || null;
+
+        calendarEvents.push(newEvent);
+    });
 
     // initialize the genetic algorithm optimizer
     const geneticOptimizer: GeneticOptimizer = new GeneticOptimizer();
 
     // start the genetic algorithm
-    const optimizedEvents: CalendarEvent[] = geneticOptimizer.optimize(events);
+    const optimizedEvents: CalendarEvent[] = geneticOptimizer.optimize(calendarEvents);
 
     // TODO: call the calendar service to save the optimized events
     
